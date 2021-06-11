b418e65e4470b9f386e6c020c196c5f2632ee8a9
alexander-svendsen <alexandersvendsen1990@gmail.com>
2021-06-11 15:03:50 +0200

    feat: add loading bar

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -7,6 +7,7 @@ export default class Preloader extends Phaser.Scene
     }
 
     preload() {
+        this.addLoading()
         this.load.image('tileset', 'tiles/tileset.png')
         this.load.tilemapTiledJSON('apartment', 'tiles/apartment.json')
 
@@ -14,10 +15,57 @@ export default class Preloader extends Phaser.Scene
         this.load.image('fridge', 'tiles/fridge.png')
         this.load.image('oven', 'tiles/oven.png')
         this.load.image('wall', 'tiles/wall.png')
-
     }
 
     create() {
         this.scene.start('game')
+    }
+
+    addLoading(){
+        const width = this.cameras.main.width;
+        const height = this.cameras.main.height;
+
+        const progressBar = this.add.graphics();
+        const progressBox = this.add.graphics();
+
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(width / 2 - 100, height / 2, 200, 28);
+        const loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 15,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                color: '#ffffff'
+            }
+        });
+
+        loadingText.setOrigin(0.5, 0.5);
+
+        const percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 15,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                color: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        this.load.on('progress', function (value) {
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            percentText.setText(parseInt(value) * 100 + '%');
+            progressBar.fillRect(width / 2 - 96, height / 2 + 4, 192 * value, 20);
+        });
+
+        this.load.once('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+        });
+
     }
 }
